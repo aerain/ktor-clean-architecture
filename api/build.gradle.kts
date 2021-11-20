b546c27a53829a2033248272aa3b968d4118c74f
aerain <j40f893@gmail.com>
2021-11-21 06:38:47 +0900

    chore: add jackson

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,3 +1,7 @@
-dependencies {
+val ktor_version: String by project
+val jackson_version: String by project
 
+dependencies {
+    implementation("io.ktor:ktor-server-core:$ktor_version")
+    implementation("com.fasterxml.jackson.core:jackson-databind:$jackson_version")
 }
