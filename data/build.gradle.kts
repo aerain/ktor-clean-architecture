f069d90607b2e93412c1ab405ee36e538614e973
aerain <j40f893@gmail.com>
2021-11-21 15:54:43 +0900

    feat: add hikariCP and mariadb jdbc

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,4 +1,14 @@
+val exposed_version: String by project
+val mariadb_version: String by project
+
 dependencies {
     api(project(":usecase"))
     api(project(":entity"))
+
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("com.zaxxer:HikariCP:5.0.0")
+    implementation("org.mariadb.jdbc:mariadb-java-client:$mariadb_version")
+
 }
--- usecase/src/main/kotlin/io/github/aerain/usecase/post/PostRepository.kt
+++ usecase/src/main/kotlin/io/github/aerain/usecase/post/PostRepository.kt
@@ -1,6 +1,7 @@
 package io.github.aerain.usecase.post
 
 import kotlinx.coroutines.flow.Flow
+import io.github.aerain.entity.post.Post
 
 interface PostRepository {
     suspend fun findAll(): Flow<Post>
